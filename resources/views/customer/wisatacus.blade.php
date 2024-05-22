45835945f86d854f3793e26674d18753ba4f6cd5
Akhii24 <emailpersonalnyaakhi@gmail.com>
2024-05-22 10:22:38 +0700

    front end dashboard,wisata

--- resources/views/customer/wisatacus.blade.php
+++ resources/views/customer/wisatacus.blade.php
@@ -4,7 +4,57 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>JemberWonder - Wisata Customer</title>
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
     <style>
+        #background{
+            background-image: url("{{ asset('img/air_terjun.jpg') }}");
+            
+        }
+        .logo img{
+            height: 95px;
+            margin-left: 10px;
+            margin-top: 10px;
+        }
+        .centered-nav{
+            display: flex;
+            justify-content: center;
+            list-style-type: none;
+            gap: 35px;
+            margin-right: 140px;
+            margin-bottom: 0px;
+            margin-top: -40px;
+        }
+        .centered-nav a{
+            text-decoration: none;
+            color: #e2e2e2;
+            font-size: 18px;
+            font-weight: bold;
+            transition: color 0.3s ease;
+        }
+        .centered-nav a:hover{
+            color:rgb( 26,47,230);
+        }
+        main{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 80vh;
+            padding: 20px;
+            color: white;
+        }
+        .images{
+            width: 60%;
+            display: flex;
+            justify-content: flex-start;
+            align-items: center;
+            position: relative;
+        }
+        #logoutForm {
+            display: flex;
+            justify-content: flex-end;
+            margin-right: 20px;
+            margin-top: -40px;
+        }
         .center {
             text-align: center;
         }
@@ -22,18 +72,32 @@
         }
     </style>
 </head>
-<body>
-    <h1>JemberWonder</h1>
-    <a href="{{ route('dashboardcustomer') }}">Dashboard</a>
-    <a href="{{ route('wisatacustomer') }}">Wisata</a>
-    <a href="{{ route('eventcustomer') }}">Event</a>
-    <a href="{{ route('masukancustomer') }}">Masukan</a>
+<header>
+    <div class="logo">
+        <img src="{{ asset('img/jwlogo.png')}}" alt="Jember Wonder">
+    </div>
+
+<nav>
+    <ul class="centered-nav">
+        <li><a href="{{ route('dashboardcustomer') }}">Dashboard</a></li>
+        <li><a href="{{ route('wisatacustomer') }}">Wisata</a></li>
+        <li><a href="{{ route('eventcustomer') }}">Event</a></li>
+        <li><a href="{{ route('masukancustomer') }}">Masukan</a></li>
+
+    </ul>
+</nav>
+<form id="logoutForm" action="{{ route('logoutcus') }}" method="POST">
+    @csrf
+    <button type="button" class="btn btn-danger" onclick="confirmLogout()">Logout</button>
+</form>
+</header>
+<div id="background"></div>
     <br>
     <br>
     <div class="center">
         <form action="{{ route('cariwisatacus') }}" method="GET">
             <input type="text" name="keyword" placeholder="Cari Wisata">
-            <button type="submit" class="btn-primary">Cari</button>
+            <button type="button" class="btn btn-primary">Cari</button>
         </form>
         @if(session('error'))
             <div class="alert alert-danger">
@@ -41,19 +105,22 @@
             </div>
         @endif
         <br>
-        <ul>
-            @foreach($wisatas as $wisata)
-            <li>
-                <a href="{{ route('detailWisataCustomer', $wisata->kd_wisata) }}">{{ $wisata->nama_wisata }}</a>
-            </li>
-            @endforeach
-        </ul>
-    </div>
+        <main>
+            <ul>
+                @foreach($wisatas as $wisata)
+                <li>
+                    <a href="{{ route('detailWisataCustomer', $wisata->kd_wisata) }}">{{ $wisata->nama_wisata }}</a>
+                </li>
+                @endforeach
+            </ul>
+            <div class="images">
+
+            </div>
+            <div class="keterangan">
+
+            </div>
+        </main>
     <br>
-    <form id="logoutForm" action="{{ route('logoutcus') }}" method="POST">
-        @csrf
-        <button type="button" onclick="confirmLogout()">Logout</button>
-    </form>
     <script>
         function confirmLogout() {
             if (confirm('Apakah Anda yakin untuk logout?')) {
